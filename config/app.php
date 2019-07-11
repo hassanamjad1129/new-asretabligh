e5b44fbfdc2de6a125992a71acca98d826658d6a
hassan <hassan@hugenet.ir>
2019-07-11 13:03:55 +0430

    added payment

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        App\Providers\GatewayServiceProvider::class,
         /*
          * Application Service Providers...
          */
