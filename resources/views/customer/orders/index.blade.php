645ff5e40231bed94878b0c297b4d79c467d6296
hassanamjad <hassan@hugenet.ir>
2019-07-16 16:26:04 +0430

    added order detail

--- resources/views/customer/orders/index.blade.php
+++ resources/views/customer/orders/index.blade.php
@@ -36,7 +36,7 @@
                             <td>{{ $order->category->name."-".$order->product->title }}</td>
                             <td>{{ ta_persian_num(jdate(strtotime($order->created_at))->format(' H:i Y/m/d')) }}</td>
                             <td>{{ ta_persian_num($order->qty) }}</td>
-                            <td>{{ ta_persian_num(number_format($order->price)) }} ریال</td>
+                            <td>{{ta_persian_num(number_format($order->getTotalPrice())) }} ریال</td>
                             <td>عادی</td>
                             <td><a href="{{ route('customer.orderDetail',[$order]) }}" style="border-radius:5px;background-image: linear-gradient(#D60000, #ee5046);color:#FFF;padding:0.2rem 1rem;margin-top: 1rem;margin-bottom: 1rem">جزییات</a></td>
                         </tr>
