f9445fae50d8b928f7bdf35e9f4d7a0cfac6ab33
hassanamjad <hassan@hugenet.ir>
2019-07-16 14:46:04 +0430

    added order detail

--- resources/views/customer/orders/detail.blade.php
+++ resources/views/customer/orders/detail.blade.php
@@ -51,14 +51,14 @@
                             $format = explode('.', $orderItem->files->front_file)[1];
                             if($format == 'pdf'){
                             ?>
-                            <a href="{{ url('/orderFiles/'.$orderItem->files->front_file) }}">
+                            <a href="{{ url('/orderFiles/'.$orderItem->files->front_file) }}" target="_blank">
                                 <img src="{{ asset('/clientAssets/img/icons8-pdf-128.png') }}" style="width: 100%"
                                      alt=""/>
                             </a>
                             <?php
                             }else{
                             ?>
-                            <a href="{{ url('/orderFiles/'.$orderItem->files->front_file) }}">
+                            <a href="{{ url('/orderFiles/'.$orderItem->files->front_file) }}" target="_blank">
                                 <img src="{{ asset('/orderFiles/'.$orderItem->files->front_file) }}" style="width: 100%"
                                      alt="">
                             </a>
