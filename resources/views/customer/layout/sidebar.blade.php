85987a944968de86bfa2ac3d4c13cf93f0037ac6
hassanamjad <hassan@hugenet.ir>
2019-07-16 16:29:21 +0430

    added order detail

--- resources/views/customer/layout/sidebar.blade.php
+++ resources/views/customer/layout/sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="customerSideBar">
     <img src="/clientAssets/img/Neutral-placeholder-profile.jpg" style="width: 100%;" alt="">
     <p style="padding: 1rem;background: #EDEDED;text-align: center">{{ auth()->guard('customer')->user()->name }}</p>
-    <li class="active"><a href="">داشبورد </a></li>
-    <li><a href="{{ route('customer.orders') }}">سفارشات</a></li>
-    <li><a href="">تنظیمات</a></li>
+    <li><a href="" style="width: 100%;display: block;">داشبورد </a></li>
+    <li><a href="{{ route('customer.orders') }}" style="width: 100%;display: block;">سفارشات</a></li>
+    <li><a href="" style="width: 100%;display: block;">تنظیمات</a></li>
 </ul>
