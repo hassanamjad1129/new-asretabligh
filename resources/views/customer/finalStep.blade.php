dded1a4b96b693f9d17895940d7a4b7fde13d541
hassanamjad <hassan@hugenet.ir>
2019-07-16 17:08:21 +0430

    added order detail

--- resources/views/customer/finalStep.blade.php
+++ resources/views/customer/finalStep.blade.php
@@ -127,9 +127,11 @@
                         <label for="shipping-{{ $shipping->id }}" style="width: 100%">
                             <div style="background:rgba(0,0,0,.1);padding:1rem;margin-top:1rem;border-radius:5px">
                                 <img src="{{ asset($shipping->icon) }}" style="width: 35%" alt="">
-                                <h4 style="display:inline-block;text-align: center;margin-top: 1rem;vertical-align: text-bottom">{{  $shipping->name }}
+                                <h4 style="display:inline-block;text-align: center;margin-top: 1rem;vertical-align:middle">{{  $shipping->name }}
                                     <br>
-                                    هزینه : {{ ta_persian_num(number_format($shipping->price)) }} ریال
+                                    <small>
+                                        هزینه : {{ ta_persian_num(number_format($shipping->price)) }} ریال
+                                    </small>
                                 </h4>
                             </div>
                         </label>
