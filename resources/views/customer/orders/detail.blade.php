48dce89b09c7ff59be0dd57493e67d02d5398933
hassanamjad <hassan@hugenet.ir>
2019-07-16 13:22:31 +0430

    added order detail

--- resources/views/customer/orders/detail.blade.php
+++ resources/views/customer/orders/detail.blade.php
@@ -6,11 +6,11 @@
         <div class="panel panel-default" id="panel">
             <div class="panel-body">
                 <div style="margin-bottom: 1rem">
-                    <h5>محصول انتخابی :</h5>
+                    <h5 style="margin-bottom: 0.5rem;font-weight: bold">محصول انتخابی :</h5>
                     <p>{{ $orderItem->category->name." ".$orderItem->product->title }}</p>
                 </div>
                 <div style="margin-bottom: 1rem">
-                    <h5>
+                    <h5 style="margin-bottom: 0.5rem;font-weight: bold">
                         مشخصات محصول :
                     </h5>
                     <p>سایز فایل : {{ $orderItem->paper->name }}</p>
@@ -18,7 +18,7 @@
                     <p>نوع سفارش : {{ $orderItem->getType() }}</p>
                 </div>
                 <div style="margin-bottom: 1rem">
-                    <h5>قیمت نهایی : </h5>
+                    <h5 style="margin-bottom: 0.5rem;font-weight: bold">قیمت نهایی : </h5>
                     <p>{{ ta_persian_num(number_format($orderItem->getTotalPrice())) }} ریال</p>
                 </div>
             </div>
