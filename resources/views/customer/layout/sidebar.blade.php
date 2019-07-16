65b57834a3525376807c461cf96f19e7f6d7a687
hassanamjad <hassan@hugenet.ir>
2019-07-16 16:32:49 +0430

    added order detail

--- resources/views/customer/layout/sidebar.blade.php
+++ resources/views/customer/layout/sidebar.blade.php
@@ -3,5 +3,5 @@
     <p style="padding: 1rem;background: #EDEDED;text-align: center">{{ auth()->guard('customer')->user()->name }}</p>
     <li><a href="/customer/home" style="width: 100%;display: block;">داشبورد </a></li>
     <li><a href="{{ route('customer.orders') }}" style="width: 100%;display: block;">سفارشات</a></li>
-    <li><a href="/customer/home" style="width: 100%;display: block;">تنظیمات</a></li>
+    <li><a href="/customer/setting" style="width: 100%;display: block;">تنظیمات</a></li>
 </ul>
