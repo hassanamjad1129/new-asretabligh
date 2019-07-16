3788c4f194d36c263c5c9bd03b83ae3c2ec6e3bc
hassanamjad <hassan@hugenet.ir>
2019-07-16 13:41:17 +0430

    added order detail

--- resources/views/customer/orders/detail.blade.php
+++ resources/views/customer/orders/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('customer.layout.dashboardMaster')
 @section('dashboardContent')
-    <h3>جزییات سفارش {{ $orderItem->id }}</h3>
+    <h4>جزییات سفارش {{ ta_persian_num($orderItem->id) }}</h4>
     <hr>
     <div class="col-xs-12">
         <div class="panel panel-default" id="panel">
@@ -14,13 +14,14 @@
                         <h5 style="margin-bottom: 0.5rem;font-weight: bold">
                             مشخصات محصول :
                         </h5>
-                        <p>سایز فایل : {{ $orderItem->getPaperName() }}</p>
-                        <p>{!! $orderItem->getData() !!} </p>
-                        <p>نوع سفارش : {{ $orderItem->getType() }}</p>
+                        <p style="line-height: 2rem;">سایز فایل : {{ $orderItem->getPaperName() }}</p>
+                        <p style="line-height: 2rem;">{!! $orderItem->getData() !!} </p>
+                        <p style="line-height: 2rem;">نوع سفارش : {{ $orderItem->getType() }}</p>
                     </div>
                     <div style="margin-bottom: 1rem">
                         <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">قیمت نهایی : </h5>
-                        <p style="display: inline-block">{{ ta_persian_num(number_format($orderItem->getTotalPrice())) }} ریال</p>
+                        <p style="display: inline-block">{{ ta_persian_num(number_format($orderItem->getTotalPrice())) }}
+                            ریال</p>
                     </div>
                 </div>
                 <div class="col-md-6">
