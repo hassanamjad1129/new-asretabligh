0cd11ae6574f87f7bb0851ee53365a8794f61e9f
hassanamjad <hassan@hugenet.ir>
2019-07-20 11:18:02 +0430

    changed qty position

--- resources/views/customer/orders/detail.blade.php
+++ resources/views/customer/orders/detail.blade.php
@@ -17,6 +17,10 @@
                         <p style="line-height: 2rem;">سایز فایل : {{ $orderItem->getPaperName() }}</p>
                         <p style="line-height: 2rem;">{!! $orderItem->getData() !!} </p>
                         <p style="line-height: 2rem;">نوع سفارش : {{ $orderItem->getType() }}</p>
+                        <div style="margin-bottom: 1rem">
+                            <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">سری سفارش :</h5>
+                            <p style="display: inline-block">{{ ta_persian_num($orderItem->qty) }}</p>
+                        </div>
                     </div>
                     <div style="margin-bottom: 1rem">
                         <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">قیمت نهایی : </h5>
@@ -38,15 +42,12 @@
     display: inline-block;
     border-radius: 2rem;">{{ $orderItem->getStatus() }}</p>
                     </div>
-                    <div style="margin-bottom: 1rem">
-                        <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">سری سفارش :</h5>
-                        <p style="display: inline-block">{{ ta_persian_num($orderItem->qty) }}</p>
-                    </div>
+
 
                     <div style="margin-bottom: 1rem">
                         <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">فایل های سفارش :</h5>
                         <div class="clearfix"></div>
-                        <div class="col-md-5" style="    padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
+                        <div class="col-md-3" style="    padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
     border-radius: 0.8rem;">
                             <?php
                             $format = explode('.', $orderItem->files->front_file)[1];
@@ -75,7 +76,7 @@
                             ?>
                         </div>
                         @if($orderItem->type=='double' and $orderItem->product->typeRelatedFile)
-                            <div class="col-md-5" style="    padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
+                            <div class="col-md-3" style="    padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
     border-radius: 0.8rem;">
                                 <?php
                                 $format = explode('.', $orderItem->files->back_file)[1];
@@ -117,100 +118,104 @@
                 <div class="col-xs-12">
                     <h4 style="font-weight: bold;margin-bottom: 1rem">خدمات پس از چاپ</h4>
                 </div>
-
-                @foreach($orderItem->services as $service)
-                    <div class="col-md-6">
-                        <div style="margin-bottom: 1rem">
-                            <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">عنوان خدمت :</h5>
-                            <p style="display: inline-block">{{ $service->service->name }}</p>
-                        </div>
-                        <div style="margin-bottom: 1rem">
-                            <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">مشخصات :</h5>
-                            <p style="line-height: 2rem">{!! $service->getData() !!}</p>
-                            @if($service->type)
-                                <p style="line-height: 2rem">نوع کار : {{ $service->getType() }}</p>
-                            @endif
-                        </div>
-
-                    </div>
-                    <div class="col-md-6">
-                        <div style="margin-bottom: 1rem">
-                            <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">هزینه خدمت :</h5>
-                            <p style="display: inline-block">{{ ta_persian_num(number_format($service->price)) }}
-                                ریال</p>
-                        </div>
-                        @if($service->type)
+                @if($orderItem->services)
+                    @foreach($orderItem->services as $service)
+                        <div class="col-md-6">
                             <div style="margin-bottom: 1rem">
-                                <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">فایل های خدمت
+                                <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">عنوان خدمت
                                     :</h5>
-                                <div class="clearfix"></div>
-                                <div class="col-md-5" style="padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
+                                <p style="display: inline-block">{{ $service->service->name }}</p>
+                            </div>
+                            <div style="margin-bottom: 1rem">
+                                <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">مشخصات :</h5>
+                                <p style="line-height: 2rem">{!! $service->getData() !!}</p>
+                                @if($service->type)
+                                    <p style="line-height: 2rem">نوع کار : {{ $service->getType() }}</p>
+                                @endif
+                            </div>
+
+                        </div>
+                        <div class="col-md-6">
+                            <div style="margin-bottom: 1rem">
+                                <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">هزینه خدمت
+                                    :</h5>
+                                <p style="display: inline-block">{{ ta_persian_num(number_format($service->price)) }}
+                                    ریال</p>
+                            </div>
+                            @if($service->type)
+                                <div style="margin-bottom: 1rem">
+                                    <h5 style="margin-bottom: 0.5rem;font-weight: bold;display: inline-block">فایل های
+                                        خدمت
+                                        :</h5>
+                                    <div class="clearfix"></div>
+                                    <div class="col-md-3" style="padding: 0.5rem;border: 1px solid rgba(0,0,0,.45);
     border-radius: 0.8rem;">
-                                    <?php
-                                    $format = explode('.', $service->files->front_file)[1];
-                                    if($format == 'pdf'){
-                                    ?>
-                                    <a href="{{ url($service->files->front_file) }}" target="_blank">
-                                        <img src="{{ asset('/clientAssets/img/icons8-pdf-128.png') }}"
-                                             style="width: 100%"
-                                             alt=""/>
-                                        <p style="text-align: center;    color: #545454;
-    font-weight: bold;
-    font-size: 1.3rem;">فایل رو</p>
-
-                                    </a>
-                                    <?php
-                                    }else{
-                                    ?>
-                                    <a href="{{ url($service->files->front_file) }}" target="_blank">
-                                        <img src="{{ asset($service->files->front_file) }}" style="width: 100%"
-                                             alt="">
-                                        <p style="text-align: center;    color: #545454;
-    font-weight: bold;
-    font-size: 1.3rem;">فایل رو</p>
-
-                                    </a>
-                                    <?php
-                                    }
-                                    ?>
-                                </div>
-                                @if($service->type=='double')
-                                    <div class="col-md-5" style="    border: 1px solid rgba(0,0,0,.45);
-    border-radius: 0.8rem;    padding: 0.5rem;">
                                         <?php
-                                        $format = explode('.', $service->files->back_file)[1];
+                                        $format = explode('.', $service->files->front_file)[1];
                                         if($format == 'pdf'){
                                         ?>
-                                        <a href="{{ url($service->files->back_file) }}"
-                                           target="_blank">
+                                        <a href="{{ url($service->files->front_file) }}" target="_blank">
                                             <img src="{{ asset('/clientAssets/img/icons8-pdf-128.png') }}"
                                                  style="width: 100%"
                                                  alt=""/>
                                             <p style="text-align: center;    color: #545454;
     font-weight: bold;
-    font-size: 1.3rem;">فایل پشت</p>
+    font-size: 1.3rem;">فایل رو</p>
+
                                         </a>
                                         <?php
                                         }else{
                                         ?>
-                                        <a href="{{ url($service->files->back_file) }}"
-                                           target="_blank">
-                                            <img src="{{ asset($service->files->back_file) }}" style="width: 100%"
+                                        <a href="{{ url($service->files->front_file) }}" target="_blank">
+                                            <img src="{{ asset($service->files->front_file) }}" style="width: 100%"
                                                  alt="">
                                             <p style="text-align: center;    color: #545454;
     font-weight: bold;
-    font-size: 1.3rem;">فایل پشت</p>
-                                        </a>
-                                        <?php } ?>
-                                    </div>
-                                @endif
-                            </div>
-                        @endif
+    font-size: 1.3rem;">فایل رو</p>
 
-                    </div>
-                    <div class="clearfix"></div>
-                    <hr>
-                @endforeach
+                                        </a>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                    @if($service->type=='double')
+                                        <div class="col-md-3" style="    border: 1px solid rgba(0,0,0,.45);
+    border-radius: 0.8rem;    padding: 0.5rem;">
+                                            <?php
+                                            $format = explode('.', $service->files->back_file)[1];
+                                            if($format == 'pdf'){
+                                            ?>
+                                            <a href="{{ url($service->files->back_file) }}"
+                                               target="_blank">
+                                                <img src="{{ asset('/clientAssets/img/icons8-pdf-128.png') }}"
+                                                     style="width: 100%"
+                                                     alt=""/>
+                                                <p style="text-align: center;    color: #545454;
+    font-weight: bold;
+    font-size: 1.3rem;">فایل پشت</p>
+                                            </a>
+                                            <?php
+                                            }else{
+                                            ?>
+                                            <a href="{{ url($service->files->back_file) }}"
+                                               target="_blank">
+                                                <img src="{{ asset($service->files->back_file) }}" style="width: 100%"
+                                                     alt="">
+                                                <p style="text-align: center;    color: #545454;
+    font-weight: bold;
+    font-size: 1.3rem;">فایل پشت</p>
+                                            </a>
+                                            <?php } ?>
+                                        </div>
+                                    @endif
+                                </div>
+                            @endif
+
+                        </div>
+                        <div class="clearfix"></div>
+                        <hr>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
