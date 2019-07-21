eb0e462191d598fe0869d8d4ed3de488215c43f3
hassanamjad <hassan@hugenet.ir>
2019-07-21 23:58:31 +0430

    added hugenet"

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>چاپ عصر تبلیغ|@yield('title')</title>
+    <title>چاپ عصر تبلیغ-@yield('title')</title>
 
     <meta name="description" content="مجری کلیه امور چاپی و تبلیغاتی
 چاپ دیجیتال-چاپ افست-خدمات دانشجویی
