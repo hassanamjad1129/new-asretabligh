c44dd0af985774dd7564a9972f0e7b32f1942049
hassanamjad <hassan@hugenet.ir>
2019-07-21 11:21:32 +0430

    added money bag report managment

--- database/migrations/2019_06_13_133914_create_customers_table.php
+++ database/migrations/2019_06_13_133914_create_customers_table.php
@@ -21,6 +21,7 @@ class CreateCustomersTable extends Migration
             $table->string('telephone', 11)->nullable();
             $table->enum('type', ['credit', 'cash'])->default('cash');
             $table->enum('gender', ['male', 'female']);
+            $table->enum('price', ['coworker', 'normal'])->default('normal');
             $table->bigInteger('credit')->default(0);
             $table->string('password');
             $table->text('address')->nullable();
