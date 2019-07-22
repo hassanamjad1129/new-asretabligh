0655861f9ff32ca2a72b988f39cda7f7bc2d0e5a
hassanamjad <hassan@hugenet.ir>
2019-07-22 14:21:53 +0430

    added hugenet"

--- resources/views/customer/auth/forgotPassword.blade.php
+++ resources/views/customer/auth/forgotPassword.blade.php
@@ -4,21 +4,19 @@
     <div class="container">
         <div class="row">
             <div class="loginWrapper">
-                <form action="">
-                    <center>
-                        <div class="col-md-6">
-                            <h3><i class="fa fa-key" style="font-size: 20px;margin-left: 6px;"></i>فراموشی رمز عبور
-                            </h3>
-                            <h5 style="margin-top: 20px">مشخصات را با دقت پر کنید</h5>
-                            <div style="margin-top:10px;width: 60%">
-                                <label for="">شماره همراه</label>
-                                <input type="text" class="form-control" name="phone"/>
-                                <button class="btn btn-danger btn-sm" style="width:25%; margin-top: 3px;float: left;">
-                                    ارسال کد فراموشی
-                                </button>
-                            </div>
+                <form action="" style="display: flex;justify-content: center;justify-items: center">
+                    <div class="col-md-6">
+                        <h3><i class="fa fa-key" style="font-size: 20px;margin-left: 6px;"></i>فراموشی رمز عبور
+                        </h3>
+                        <h5 style="margin-top: 20px">مشخصات را با دقت پر کنید</h5>
+                        <div style="margin-top:10px;width: 60%">
+                            <label for="">شماره همراه</label>
+                            <input type="text" class="form-control" name="phone"/>
+                            <button class="btn btn-danger btn-sm" style="width:25%; margin-top: 3px;float: left;">
+                                ارسال کد فراموشی
+                            </button>
                         </div>
-                    </center>
+                    </div>
                 </form>
             </div>
         </div>
