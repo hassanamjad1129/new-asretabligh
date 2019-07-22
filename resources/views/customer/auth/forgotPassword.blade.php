4415ed3eb6360b46b07024d8f5a53049e440f0ef
hassanamjad <hassan@hugenet.ir>
2019-07-22 14:27:05 +0430

    added hugenet"

--- resources/views/customer/auth/forgotPassword.blade.php
+++ resources/views/customer/auth/forgotPassword.blade.php
@@ -5,11 +5,12 @@
         <div class="row">
             <div class="loginWrapper">
                 <form action="" style="display: flex;justify-content: center;justify-items: center">
-                    <div class="col-md-6">
-                        <h3><i class="fa fa-key" style="font-size: 20px;margin-left: 6px;"></i>فراموشی رمز عبور
+                    <div class="col-md-5">
+                        <h3 style="text-align: center"><i class="fa fa-key"
+                                                          style="font-size: 20px;margin-left: 6px;"></i>فراموشی رمز عبور
                         </h3>
-                        <h5 style="margin-top: 20px">مشخصات را با دقت پر کنید</h5>
-                        <div style="margin-top:10px;width: 60%">
+                        <h5 style="text-align: center;margin-top: 20px">مشخصات را با دقت پر کنید</h5>
+                        <div style="margin-top:10px;width: 100%">
                             <label for="">شماره همراه</label>
                             <input type="text" class="form-control" name="phone"/>
                             <button class="btn btn-danger btn-sm" style="width:25%; margin-top: 3px;float: left;">
