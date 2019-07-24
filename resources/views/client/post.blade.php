fd103d4d0133db920213327d913def9be807662a
hassanamjad <hassan@hugenet.ir>
2019-07-24 19:04:16 +0430

    added redirect asretabligh to chapasretabligh

--- resources/views/client/post.blade.php
+++ resources/views/client/post.blade.php
@@ -2,16 +2,10 @@
 @section('title'){{ $post->title }}@endsection
 @section('content')
     <div class="container">
-        <div class="col-md-9">
-            <img src="{{ asset($post->picture) }}"
-                 style="width: 80%;border-radius: 1rem;box-shadow: 0 0 5px rgba(0,0,0,.1)" alt="">
-            <h1 style="font-size: 2rem;margin-bottom: 1rem;margin-top: 1rem">{{ $post->title }}</h1>
-            {!! $post->description !!}
-        </div>
         <div class="col-md-3">
             <div class="panel panel-default" id="postCategories">
                 <div class="panel-heading">
-                    <h5>دسته بندی پست ها</h5>
+                    <h5>دسته بندی</h5>
                 </div>
                 <div class="panel-body">
                     <ul>
@@ -45,5 +39,31 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-9">
+            <div class="col-md-10">
+                <img src="{{ asset($post->picture) }}"
+                     style="width: 95%;border-radius: 1rem;box-shadow: 0 0 5px rgba(0,0,0,.1)" alt="">
+            </div>
+            <div class="col-md-2">
+                <div class="seenBox">
+                    <div class="seenBoxHeading"> {{ jdate(strtotime($post->created_at))->format('l') }}</div>
+                    <div class="seenBoxBody">
+                        {{ jdate(strtotime($post->created_at))->format('j F') }}
+                    </div>
+                </div>
+                <div class="seenBox">
+                    <div class="seenBoxHeading"> بازدید</div>
+                    <div class="seenBoxBody">
+                        <span>{{ $post->seen }}</span>
+                        <span><i class="fa fa-eye"></i></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12" style="    line-height: 2.2rem;">
+                <h1 style="font-size: 2rem;margin-bottom: 1rem;margin-top: 1rem">{{ $post->title }}</h1>
+                {!! $post->description !!}
+            </div>
+        </div>
+
     </div>
 @endsection
