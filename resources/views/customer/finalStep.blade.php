10e4762eb74398a1d79cd6c585d60cda7dbc44b8
hassanamjad <hassan@hugenet.ir>
2019-07-27 14:11:12 +0430

    added service price delimiter

--- resources/views/customer/finalStep.blade.php
+++ resources/views/customer/finalStep.blade.php
@@ -146,13 +146,14 @@
                                            id="shipping-{{ $shipping->id }}">
                                     <label for="shipping-{{ $shipping->id }}" style="width: 100%">
                                         <div style="background:rgba(0,0,0,.1);padding:1rem;margin-top:1rem;border-radius:5px">
-                                            <img src="{{ asset($shipping->icon) }}" style="width: 35%" alt="">
                                             <h4 style="display:inline-block;text-align: center;    line-height: 3rem;vertical-align:middle">{{  $shipping->name }}
                                                 <br>
                                                 <small>
                                                     هزینه : {{ ta_persian_num(number_format($shipping->price)) }} ریال
                                                 </small>
                                             </h4>
+                                            <img src="{{ asset($shipping->icon) }}" style="width: 35%" alt="">
+
                                         </div>
                                     </label>
 
