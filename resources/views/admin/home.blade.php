e9c5f886c882c81d59e3985ab0213cf081b4e819
hassanamjad <hassan@hugenet.ir>
2019-07-28 11:13:56 +0430

    added update admin profile

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-10 col-md-offset-2">
                 <div class="card">
                     <div class="card-header">داشبورد</div>
                     <div class="card-block">
