af4adec27594b908f422e2e2326d8750a46c481b
hassanamjad <hassan@hugenet.ir>
2019-08-25 16:05:15 +0430

    changed pricelist style

--- resources/views/categoryProductPrice.blade.php
+++ resources/views/categoryProductPrice.blade.php
@@ -8,28 +8,66 @@
             background: #d60000;
             color: #FFF;
             padding: 1rem !important;
+
         }
 
         .table-bordered > thead > tr > th, .table-bordered > tbody > tr > th, .table-bordered > tfoot > tr > th, .table-bordered > thead > tr > td, .table-bordered > tbody > tr > td, .table-bordered > tfoot > tr > td {
             border: 1px solid #ddd;
             padding: 0.5rem 1rem;
+            text-align: center;
+        }
+
+        .nav-tabs > li.active > a, .nav-tabs > li.active > a:hover, .nav-tabs > li.active > a:focus {
+            color: #fff;
+            cursor: default;
+            background-color: #D60000;
+            border: 1px solid #ddd;
+            border-radius: 2rem;
+        }
+
+        .nav-tabs > li > a:hover {
+            border-color: #eee #eee #ddd;
+        }
+        .nav > li > a:hover, .nav > li > a:focus {
+            text-decoration: none;
+            background-color: #d60000;
+            color: #FFF;
+            border-radius: 2rem;
+        }
+
+        .nav-tabs > li > a {
+            margin-left: auto;
+            margin-right: -2px;
+            border-radius: 4px 4px 0 0;
+            margin: 0 0.4rem;
+        }
+
+        h2{
+            font-size: 1.7rem;
+        }
+
+        .table-bordered > tbody > tr:hover {
+            transform: scale(1.02);
+            transition: all 0.3s ease 0s;
         }
     </style>
     <div class="container">
         <h1 style="text-align: center">لیست قیمت {{ $category->name }}</h1>
         <br>
-        <ul class="nav nav-tabs">
+        <ul class="nav nav-tabs" style="display: flex;
+    justify-content: center;
+    border: none;">
             <?php $i = 0;?>
             @foreach($products as $product)
                 <?php $i++;?>
-                <li class="{{$i==1?'active':''}}"><a data-toggle="tab" href="#{{$product->id}}"
-                                                     onclick="tabActive({{$product->id}})">{{$product->title}}</a></li>
+                <li class="{{$i==1?'active':''}}"><a data-toggle="tab" href="#c{{$product->id}}">{{$product->title}}</a>
+                </li>
             @endforeach
         </ul>
         <div class="tab-content">
             <?php use App\Models\ProductValue as ProductValueAlias;$i = 1; ?>
             @foreach($products as $product)
-                <div id="{{$product->id}}" class="tab-pane fade{{$i==1?' in active':''}}">
+                <div id="c{{$product->id}}" class="tab-pane fade{{$i==1?' in active':''}}">
                     <h2 style="text-align: center;margin-top: 20px;">لیست قیمت <a
                                 href="{{ route('showProduct',$product) }}">{{ $product->title }}</a></h2>
                     <br>
@@ -117,11 +155,5 @@
     </div>
 @endsection
 @section('extraScripts')
-    <script>
-        function tabActive(tagId) {
-            $('.tab-content').children().removeClass(' in active');
-            $('#' + tagId).addClass("tab-pane fade in active");
-        }
-    </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 @endsection
