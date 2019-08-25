168e35c49fff495a15a0322a42f96902de49af80
hassanamjad <hassan@hugenet.ir>
2019-08-25 18:52:30 +0430

    changed price list table style

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -125,7 +125,7 @@
                                         <img src='{{ url('/getProductThumbnailPicture/'.$product->id) }}'>
                                         <div class="overlay">
                                             <a href="{{ route('showProduct',[$product]) }}">
-                                                <h2>ثبت سفارش</h2></a>
+                                                <h2 style="padding: 0.5rem;">ثبت سفارش</h2></a>
                                             <i class="flaticon-mark"></i>
                                         </div>
                                     </div>
